41b0833bf6707a474f258a0239c64b6fbfea2aef
merajkhan <khanmeraj83@gmail.com>
2024-06-03 16:36:00 +0500

    deployement fix in nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,28 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
+interface Credentials {
+  username: string;
+  password: string;
+}
+
+interface User {
+  id: string; // Make sure id is of type string
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface Token {
+  id: string; // Make sure id is of type string
+}
+
+interface Session {
+  user: {
+    id: string; // Make sure id is of type string
+  };
+}
+
 export default NextAuth({
   providers: [
     CredentialsProvider({
@@ -9,30 +31,28 @@ export default NextAuth({
         username: { label: 'Username', type: 'text' },
         password: { label: 'Password', type: 'password' },
       },
-      authorize(credentials) {
-        const user = { id: 1, name: 'admin', email: 'admin@gmail.com', password: 'admin123' };
+      async authorize(credentials: Credentials) {
+        const user: User = { id: '1', name: 'admin', email: 'admin@gmail.com', password: 'admin123' }; // Ensure id is of type string
         if (credentials?.username === 'admin' && credentials?.password === 'admin123') {
-          return user;
+          return Promise.resolve(user);
         }
-        return null;
+        return Promise.resolve(null);
       },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async jwt({token, user}) {
+    async jwt({ token, user }: { token: Token; user?: User }) {
       if (user) {
         token.id = user.id;
       }
-      return token;
+      return Promise.resolve(token);
     },
-    async session({session, token}) {
-        if (token) {
-            session.user.id = token.id;
-        }
-      return session;
+    async session({ session, token }: { session: Session; token: Token }) {
+      if (token) {
+        session.user.id = token.id;
+      }
+      return Promise.resolve(session);
     },
   },
 });
-
-
