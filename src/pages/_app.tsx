df1f77bce598eda2d81f742459f0fef8405770fd
merajkhan <khanmeraj83@gmail.com>
2024-06-03 15:51:49 +0500

    deployement issue fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import '@/styles/globals.css';
 
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import Navbar from '@/components/Navbar';
 
-const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => (
-  <SessionProvider session={session}>
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <SessionProvider session={pageProps.session}>
     <div className=''>
       <Navbar />
       <main className='p-6'>
@@ -15,5 +16,3 @@ const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => (
 );
 
 export default MyApp;
-
- 
